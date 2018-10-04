e901e681e0195ff56263818800a6f447752d3873
cazador <pradeepmudvari066@gmail.com>
2018-10-04 11:46:37 +0545

    new Project

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends ('layouts.app')
+
+@section('content')
+	<class="jumbotron text-center">
+	<h1>Welcome to laravel.</h1>
+	<p>This is the new app.</p>
+	<p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-sucess btn-lg" href="/register" role="button">Register</a></p>
+	<!-- <h1>{{$title}}</h1> 
+	<p>This is the laravel application.</p> -->
+@endsection 
